0d89cd319c51c6a3ad4392cc0752690697c513b6
Sergi Garcia <sergigarciapena@gmail.com>
2017-03-10 11:52:50 +0100

    first approach to obstacles, goForward and goBackwards changed

--- mars_rover/rover.js
+++ mars_rover/rover.js
@@ -1,20 +1,22 @@
 
 //Description grid 10x10 (rosMax*colsMax)
-/*
-              S
-    0,0 0,1 0,2 0,3......0,9
-    1,0 1,1 1,2 1,3......1,9
-  E ......................... W
-    .........................
-    9,0 9,1 9,2 9,3......9,9
-              N
+/*               (y-)
+                 S
+       0,0 0,1 0,2 0,3......0,9
+       1,0 1,1 1,2 1,3......1,9
+(x-)E  ......................... W (x +)
+       .........................
+       9,0 9,1 9,2 9,3......9,9
+                 N
+                (y+)
 */
 
 //In chrome console introduce commandExecution('fffff'); for example, and resetPositionDirection(); to reset the rover's position
 
 var myRover = {
   position: [0,0], //0-->rows, 1-->cols
-  direction: 'N'
+  direction: 'N',
+  lastPosition: this.position
 };
 
 var rowsMax = 10;
@@ -24,72 +26,173 @@ var grid = [];
 for (i=0;i<rowsMax;i++) {
   grid[i]=[];
   for (j=0;j<colsMax;j++) {
-    grid[i][j]=0;
+    grid[i][j]=1;
   }
+}
+
+function placeObstacles()
+{
+
 }
 
 function goForward(rover) {
 
   switch(rover.direction) {
     case 'N':
-      if(++(rover.position[0])%rowsMax===0) //go y+
+      rover.lastPosition = rover.position;
+      rover.position[0]++;
+      if((rover.position[0])%rowsMax===0) //go y+
       {
         rover.position[0]=0;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
     case 'W':
-      if(++(rover.position[1])%colsMax===0) //go x+
+      rover.lastPosition = rover.position;
+      rover.position[1]++;
+      if((rover.position[1])%colsMax===0) //go x+
       {
         rover.position[1]=0;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
     case 'S':
-      if(--(rover.position[0])===-1) //go y-
+      rover.lastPosition = rover.position;
+      rover.position[0]--;
+      if((rover.position[0])===-1) //go y-
       {
         rover.position[0]=rowsMax-1;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
     case 'E':
-      if(--(rover.position[1])===-1) //go x-
+      rover.lastPosition = rover.position;
+      rover.position[1]--;
+      if((rover.position[1])===-1) //go x-
       {
         rover.position[1]=colsMax-1;
+      }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
       }
       break;
   }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+
 }
 
 function goBackwards(rover) {
 
   switch(rover.direction) {
     case 'N':
-      if(--(rover.position[0])===-1) //go y-
+      rover.lastPosition = rover.position;
+      rover.position[0]--;
+      if((rover.position[0])===-1) //go y-
       {
         rover.position[0]=rowsMax-1;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
     case 'W':
-      if(--(rover.position[1])===-1) //go x-
+      rover.lastPosition = rover.position;
+      rover.position[1]--;
+      if((rover.position[1])===-1) //go x-
       {
         rover.position[1]=colsMax-1;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
     case 'S':
-      if(++(rover.position[0])%rowsMax===0) //go y+
+      rover.lastPosition = rover.position;
+      rover.position[0]--;
+      if((rover.position[0])%rowsMax===0) //go y+
       {
         rover.position[0]=0;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
     case 'E':
-      if(++(rover.position[1])%colsMax===0) //go x+
+      rover.lastPosition = rover.position;
+      rover.position[1]--;
+      if((rover.position[1])%colsMax===0) //go x+
       {
         rover.position[1]=0;
       }
+      console.log("Value grid " + grid[rover.position[0]][rover.position[1]]);
+      if(grid[rover.position[0]][rover.position[1]] === 1)
+      {
+        console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
+      else {
+        rover.position = rover.lastPosition;
+        console.log("Bumped into an obstacle");
+        console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
+      }
       break;
   }
-
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]; Direction: " + rover.direction);
 }
 
 function turnRight(rover) {
@@ -169,15 +272,17 @@ function resetPositionDirection()
   return true;
 }
 
+
 //Description grid 10x10 (rosMax*colsMax)
-/*
-              S
-    0,0 0,1 0,2 0,3......0,9
-    1,0 1,1 1,2 1,3......1,9
-  E ......................... W
-    .........................
-    9,0 9,1 9,2 9,3......9,9
-              N
+/*               (y-)
+                 S
+       0,0 0,1 0,2 0,3......0,9
+       1,0 1,1 1,2 1,3......1,9
+(x-)E  ......................... W (x +)
+       .........................
+       9,0 9,1 9,2 9,3......9,9
+                 N
+                (y+)
 */
 
 //In chrome console introduce commandExecution('fffff'); for example, and resetPositionDirection(); to reset the rover's position
